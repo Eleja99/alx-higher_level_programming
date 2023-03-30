3de2efd85180a75728c4f2ae0bb1e7f91ad5b12b
eleja99 <elejaabdqadr@gmail.com>
2023-03-30 22:23:30 +0000

    Python conditional and loop tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-if int(number[-1]) > 5:
-	print(f'Last digit of {int(number)} is {int(number[-1])} and is greater than 5')
-elif int(number[-1]) == 0:
-	print(f'Last digit of {int(number)} is {int(number[-1])} and is 0')
-elif int(number[-1]) < 6 and int(number[-1] != 0):
-	print(f'Last digit of {int(number)} is {int(number[-1])} and is less than 6 and not 0')
+lastDigit = 0
 
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = (-number % 10) * -1
 
+message = f"Last digit of {number} is {lastDigit}"
+
+if lastDigit == 0:
+    print(f"{message} and is 0")
+elif lastDigit > 5 and lastDigit % 10 != 0:
+    print(f"{message} and is greater than 5")
+else:
+    print(f"{message} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
-for i in range (100):
-	if i == 99:
-		print(i)
-	else:
-		print(f"{'0' + str(i)}" if i < 10 else f"{i}" , end = ', ')
-		#print('0' + str(i) if i < 10 else i, end = ', ')
+#!/usr/bin/python3
+for i in range(100):
+    if i == 99:
+        print(i)
+    else:
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")
